1d7587a5fc63b3d7fe5b73a0c0febfe17e8a519f
czachariah <zachariahchris@yahoo.com>
2020-10-26 09:07:11 -0400

    started the first part

---
+++ vencrypt.java
@@ -0,0 +1,16 @@
+/**
+ * This is the vencrypt class.
+ * 
+ * Usage:
+ *  java vencrypt keyFile plainText cipherFile
+ * 
+ * Obtains key data from the keyFile.
+ * Encrypts the plainText and writes it into the cipherFile.
+ * 
+ * @author Chris Zachariah (cvz2)
+ */
+public class vencrypt {
+    public static void main(String[] args) {
+
+    } // ends the main()
+} // ends the vencrypt class
